580c0e5951ca4d1cfa2d41121054438600283b10
SrTomson <74432095+SrTomson@users.noreply.github.com>
2021-10-29 12:53:14 -0300

    back to first working script

--- gatitos.js
+++ gatitos.js
@@ -1,50 +1,30 @@
 const apiKey = 'U1nx7WliKVvvfuiMA0bnRDWGGT3EQjJx';
-let estado = {
-    indexUsados: []
-};
-
 let contenedor = document.getElementById('gatitos-contenedor');
 let img = document.getElementById('gatitos-imagen');
 let btn = document.getElementById('boton-buscar');
 let url;
 
-function stickerOrGif() {
+function stickerOrGif(){
     let random = Math.floor(Math.random() * 10);
-    if (random > 00 && random <= 5) {
+    console.log(random);
+    // Math.floor(random)
+    if(random > 00 && random <= 5){
         url = `http://api.giphy.com/v1/stickers/search?api_key=${apiKey}&q=cat&limit=50`;
     }
-    else if (random > 5 && random <= 10) {
+    else if(random > 5 && random <= 10){
         url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cat&limit=50`
     }
 }
 
-function randomGifIndex(data) {
-    let i = Math.floor(Math.random() * data);
-    return i;
-}
-
-let traerRandom = function () {
+let traerRandom = function() {
     stickerOrGif();
     console.log(url);
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            let randomIndex = randomGifIndex(data.data.length);
-            estado.indexUsados.push(randomIndex);
-
-            for (let i = 0; i < estado.indexUsados.length; i++) {
-                if (i != randomIndex) {
-                    img.src = data.data[i].images.downsized.url;
-                    contenedor.appendChild(img);
-                }
-                else{
-                    traerRandom();
-                    break;
-                }
-            }
-
-            console.log('estado: ', estado.indexUsados);
-
+            let randomGifIndex = Math.floor(Math.random() * data.data.length)
+            img.src = data.data[randomGifIndex].images.downsized.url;
+            contenedor.appendChild(img);
         })
         .catch(error => {
             console.error(error);
@@ -56,4 +36,3 @@ btn.addEventListener('click', e => {
 })
 
 traerRandom();
-
